18c1c8d7226a2d3b8e88bf686400947db9400cbc
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2022-03-18 16:00:03 +0800

    ROG update
    Add UI buttons to control opne/close fog and snow;
    Add terrain material change from grass to snow surface;

--- Outdoor/Assets/Scripts/GameManager.cs
+++ Outdoor/Assets/Scripts/GameManager.cs
@@ -12,6 +12,12 @@ public class GameManager : MonoBehaviour
     public Material winter;
     public Text csbText;
     public GameObject rain;
+    public GameObject fog;
+    public GameObject snow;
+    public Terrain[] terrain;
+    public Material snowGround;
+    public Material grassGround;
+    public GameObject light;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +29,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rain.activeSelf || fog.activeSelf || snow.activeSelf)
+        {
+            light.transform.localRotation = Quaternion.Euler(new Vector3(200, 90, 90));
+        }
+        else
+        {
+            light.transform.localRotation = Quaternion.Euler(new Vector3(90, 90, 90));
+
+        }
     }
 
     public void OpenSettings()
@@ -42,10 +56,12 @@ public class GameManager : MonoBehaviour
         if (RenderSettings.skybox == summer)
         {
             RenderSettings.skybox = winter;
+            ChangeGround();
         }
         else
         {
             RenderSettings.skybox = summer;
+            ChangeGround();
         }
 
     }
@@ -78,5 +94,43 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void Fog()
+    {
+        if (!fog.activeSelf)
+        {
+            fog.SetActive(true);
+        }
+        else
+        {
+            fog.SetActive(false);
+        }
+    }
+
+    public void Snow()
+    {
+        if (!snow.activeSelf)
+        {
+            snow.SetActive(true);
+        }
+        else
+        {
+            snow.SetActive(false);
+        }
+    }
+
+    public void ChangeGround()
+    {
+        foreach (Terrain t in terrain)
+        {
+            if (t.materialTemplate == grassGround)
+            {
+                t.materialTemplate = snowGround;
+            }
+            else
+            {
+                t.materialTemplate = grassGround;
+            }
+        }
+    }
 
 }
